83911632f963c1a4e5973035bd476c1d7133cb75
Seyi Samuel <Seyi.Samuel10@yahoo.com>
2022-08-04 14:32:47 +0100

    Commenting the code

--- js/main.js
+++ js/main.js
@@ -3,16 +3,21 @@ const userInput = document.querySelector("#text-zone");
 const countDisplay = document.querySelector("#count-increment");
 
 const vowelCount = () => {
+    // store what the user enters on the textarea
     let valueStr = userInput.value.toLowerCase();
    
+    // store the vowels in an array
     let arrVowel = ['a', 'e', 'i', 'o', 'u']; 
     let counter = 0;
+    // Loop through the text entered
     for(let i = 0; i < valueStr.length; i++) {
+        // Check if the alphabet's vowels were inputed
         if(arrVowel.includes(valueStr[i])) {
             console.log("i: " + valueStr[i]);
             counter++;
         }
     }
+    // Show the number incresing as the user enters a vowel
     countDisplay.innerText = counter;
     valueStr = "";
     return counter;
