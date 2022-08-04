563b21430af5cbf3c861f2ed4c81b9daaef7353b
Seyi Samuel <Seyi.Samuel10@yahoo.com>
2022-08-04 14:22:59 +0100

    Create vowelCount function to count the vowels typed by user in the browser

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@ const userInput = document.querySelector("#text-zone");
 
 const countDisplay = document.querySelector("#count-increment");
 
+const vowelCount = () => {
+    let valueStr = userInput.value.toLowerCase();
+   
+    let arrVowel = ['a', 'e', 'i', 'o', 'u']; 
+    let counter = 0;
+    for(let i = 0; i < valueStr.length; i++) {
+        if(arrVowel.includes(valueStr[i])) {
+            console.log("i: " + valueStr[i]);
+            counter++;
+        }
+    }
+    countDisplay.innerText = counter;
+    valueStr = "";
+    return counter;
+}
+
